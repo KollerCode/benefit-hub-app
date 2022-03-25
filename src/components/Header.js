81f0bd7502e681621697d6ab95a6b4127217ab62
Natasha Koller <kollernatasha@gmail.com>
2022-03-25 14:57:11 -0400

    added header and cleaned up some of the pre-added react components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1 id="header-title">Task Lister</h1>
+        </div>
+    )
+}
+
+export default Header
