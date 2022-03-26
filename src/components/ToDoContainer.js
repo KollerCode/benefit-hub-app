8d492cab2e7bb55ed55a56462708467d7e088934
Natasha Koller <kollernatasha@gmail.com>
2022-03-25 22:54:35 -0400

    working on the sorting for the app and trying to decide between a toggle or just two seperate buttons

--- src/components/ToDoContainer.js
+++ src/components/ToDoContainer.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import NewToDo from "./NewToDo";
 import ToDos from "./ToDos";
+import Search from "./Search";
 
-function ToDoContainer({ search }) {
+function ToDoContainer() {
     const [toDos, setToDos] = useState([])
-    const [sortBy, setSortBy] = useState ("id")
+    const [sortByAscending, setSortAscending] = useState("title")
+    const [sortByDescending, setSortDescending] = useState("title");
     
     function deleteToDo(id) {
       const updatedToDos = toDos.filter(
@@ -21,18 +23,35 @@ function ToDoContainer({ search }) {
     const filteredToDos = toDos.filter((toDos) => {
       return toDos.title
     });
+    console.log(filteredToDos)
 
-    const sortedToDos = filteredToDos.sort((a, b) => {
-        const toDoA = a.name; 
-        const toDoB = b.name; 
-        if (toDoA < toDoB) {
+    const sortedToDos = filteredToDos.sort((a, z) => {
+        const aToDo = a.title; 
+        const zToDo = z.title; 
+        if (aToDo < zToDo) {
             return -1;
         }
-        if (toDoA > toDoB) {
+        if (aToDo > zToDo) {
             return 1;
         }
         return 0;
     });
+    console.log(sortedToDos)
+    const toggleSort = () => {
+
+    }
+
+    const sortAscending = () => {
+        sortedToDos.sort((a, b) => a - b);   
+        setSortAscending((sortByAscending) => !sortByAscending);
+    }
+    console.log(sortAscending)
+    
+    const sortDescending = () => {
+        filteredToDos.sort((a, b) => a - b).reverse();
+        setSortDescending((sortByDescending) => !sortByDescending)
+    }
+
 
     useEffect(() => {
       fetch("https://jsonplaceholder.typicode.com/todos")
@@ -40,7 +59,7 @@ function ToDoContainer({ search }) {
         .then((toDos) => setToDos(toDos));
     }, []);
 
-    const listedToDos = sortedToDos.map((todo) => {
+    const listedToDos = toDos.map((todo) => {
       return <ToDos
           key={todo.id}
           todo={todo}
@@ -50,11 +69,14 @@ function ToDoContainer({ search }) {
     });
 
     return (
-        <main>
-            <ul className="list">{listedToDos}</ul>
-            <NewToDo addToDo={addToDo}/>
-        </main>
-    )
+      <main>
+        <NewToDo addToDo={addToDo} />
+        <Search />
+        <button onClick={sortAscending}>asc</button>
+        <button onClick={sortDescending}>desc</button>
+        <ul className="list">{listedToDos}</ul>
+      </main>
+    );
 }
 
 export default ToDoContainer;
